a2f4075a1e0a5b8ea3419fd6a8700124c695654e
Evan Christian <evan.chrs23@gmail.com>
2025-11-25 23:12:13 +0800

    initial commit

--- AIT2509152/test03.py
+++ AIT2509152/test03.py
@@ -1,7 +1,10 @@
-n = 5
+from sys import argv
+
+script, n, x = argv
+total = 0
 
 for i in range(n):
-    print(" "*(n-i+1), "*"*(2*i+1))
-for i in range(n-1, 0, -1):
-    print(" "*(n-i), "*"*((2)*(i+1)-1))
+    add = ((-1)**i)*(x**(i+1))/(i+1)
+    total += add
 
+print(total)
