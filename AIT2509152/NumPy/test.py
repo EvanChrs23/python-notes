b33d1817549f3359ba62db4a065161626dbeea69
Evan Christian <evan.chrs23@gmail.com>
2025-11-29 03:13:19 +0800

    initial commit

--- AIT2509152/NumPy/test.py
+++ AIT2509152/NumPy/test.py
@@ -1,7 +1,7 @@
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL']='3'
 import tensorflow as tf
+import numpy as np
+import pandas as pd
 
-a = tf.constant([1, 2, 3])
-b = tf.constant([4, 5, 6])
-print(a * b)
+print(pd.__version__)
