0742e57ef0fd50f26cc430a2c6b61325b9adfff2
Evan Christian <evan.chrs23@gmail.com>
2025-11-28 12:16:50 +0800

    initial commit

--- AIT2509152/test03.py
+++ AIT2509152/test03.py
@@ -5,8 +5,8 @@ n = int(power)
 x = int(number)
 total = 0
 
-for i in range(n):
-    add = ((-1)**i)*(x**(i+1))/(i+1)
+for i in range(int(n)):
+    add = ((-1)**i)*(int(x)**(i+1))/(i+1)
     total += add
 
 print(total)
